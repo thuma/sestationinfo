7a6828d4342ff6e7d73bb7ad5c78475b59852d3f
Martin Thuresson <martin.thure@gmail.com>
2014-10-31 12:48:46 +0100

    added selections

--- getbypos.py
+++ getbypos.py
@@ -1,4 +1,7 @@
 import LatLon
+import tornado.ioloop
+import tornado.web
+
 ins = open("../Transit-Stop-Identifier-Conversions-Sweden/stops.txt", "r" )
 ids = []
 for line in ins:
@@ -9,12 +12,34 @@ for line in ins:
       error = 1
 ins.close()
 
-latin = 59.3284697
-lonin = 18.0617493
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+      done = []
+      try:
+        latin = float(self.get_argument('lat'))
+        lonin = float(self.get_argument('lon'))
+        distin = float(self.get_argument('distance',5000))
+        limit = int(self.get_argument('limit',100))
+        start = LatLon.LatLon(latin,lonin)
+      except:
+        self.write({"error":"malformed request must be formated like this example ?lat=15.2&long=60.0 , optional: &distance=10000 in meters. Good values are 1 to 10000 default 5000 meters, &limit=2 number of stops to show default 100"})
+        self.finish()
+      for line in ids:
+        if abs(latin-line["lat"]) < 1 and abs(lonin-line["lon"]) < 1:
+          distance = LatLon.LatLon(line["lat"], line["lon"]).distance(start)*1000
+          if distance < distin:
+            line["distance"] = distance
+            done.append(line)
+      self.write({"closesto":sorted(done, key=lambda row: row["distance"])[:limit]})
 
-start = LatLon.LatLon(latin,lonin)
-for line in ids:
-  if abs(latin-line["lat"]) < 1 and abs(lonin-line["lon"]) < 1:
-    distance = LatLon.LatLon(line["lat"], line["lon"]).distance(start)*1000
-    if distance < 5000:
-      print str(distance) + "m, id:"+line["id"]
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(9090)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+
+
