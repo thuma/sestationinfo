9bb3705f95ab1eccbf4662603caed270a60f02fb
Martin Thuresson <martin.thure@gmail.com>
2014-10-31 07:26:35 +0100

    first working version

--- getbypos.py
+++ getbypos.py
@@ -2,13 +2,19 @@ import LatLon
 ins = open("../Transit-Stop-Identifier-Conversions-Sweden/stops.txt", "r" )
 ids = []
 for line in ins:
-    parts = line.split(',')
-    if parts[0] != "stopid":
-    	ids.append({"id":parts[0],"lon":parts[2],"lat":parts[3]})
+    parts = line.strip().split(',')
+    try:
+      ids.append({"id":parts[0],"lat":float(parts[2]),"lon":float(parts[3])})
+    except:
+      error = 1
 ins.close()
 
-start = LatLon.LatLon(60.0, 16.0)
-for line in parts:
-	distance = LatLon.LatLon(line["lat"], line["lon"]).distance(start)
-	print distance
-	
+latin = 59.3284697
+lonin = 18.0617493
+
+start = LatLon.LatLon(latin,lonin)
+for line in ids:
+  if abs(latin-line["lat"]) < 1 and abs(lonin-line["lon"]) < 1:
+    distance = LatLon.LatLon(line["lat"], line["lon"]).distance(start)*1000
+    if distance < 5000:
+      print str(distance) + "m, id:"+line["id"]
